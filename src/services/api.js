49caf7d3bef79eb939d332045fd829904c0f5230
lfnsyhd <alfansyahadahu@gmail.com>
2025-12-27 18:49:26 +0700

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://service-finance-4yk16s85v-lfnsyhds-projects.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
